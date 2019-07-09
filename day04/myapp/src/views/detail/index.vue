2b5b0df4f0bcd4a270dbd65152f4adbd12d266ca
wudaxun <522864637@qq.com>
2019-07-09 10:28:23 +0800

    上拉加载下拉刷新

--- day04/myapp/src/views/detail/index.vue
+++ day04/myapp/src/views/detail/index.vue
@@ -1,23 +1,136 @@
 <template>
   <div class="box">
     <div class="container">
-      <header class="header">详情头部</header>
+      <van-nav-bar
+        :title="title"
+        left-text="返回"
+        right-text="分享"
+        left-arrow
+        @click-left="back"
+        @click-right="share"
+      />
       <div class="content">
         {{ title }}
         <Rating :rating="(rating / 2).toFixed(1)"/>
       </div>
     </div>
-    <footer class="footer">详情底部</footer>
+    <van-goods-action>
+      <van-goods-action-icon
+        icon="chat-o"
+        text="客服"
+        @click="chat"
+      />
+      <van-goods-action-icon
+        info="5"
+        icon="cart-o"
+        text="购物车"
+        @click="goCart"
+      />
+      <van-goods-action-icon
+        icon="shop-o"
+        text="店铺"
+        @click="goShop"
+      />
+      <van-goods-action-button
+        type="warning"
+        text="加入购物车"
+        @click="addCart"
+      />
+      <van-goods-action-button
+        type="danger"
+        text="立即购买"
+        @click="buy"
+      />
+    </van-goods-action>
+    <van-sku
+      v-model="show"
+      stepper-title="我要买"
+      :sku="sku"
+      :goods="goods"
+      show-add-cart-btn
+      reset-stepper-on-hide
+      :initial-sku="initialSku"
+    >
+    </van-sku>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import { GoodsAction, GoodsActionIcon, GoodsActionButton, Sku, NavBar } from 'vant'
 import Rating from '@/components/common/Rating'
+
+Vue.use(GoodsAction).use(GoodsActionIcon).use(GoodsActionButton)
+Vue.use(Sku)
+Vue.use(NavBar)
 export default {
   data () {
     return {
       title: '',
-      rating: ''
+      rating: '',
+      show: false,
+      goods: {
+        // 商品标题
+        title: '测试商品',
+        // 默认商品 sku 缩略图
+        picture: 'https://img.yzcdn.cn/1.jpg'
+      },
+      initialSku: {
+        // 键：skuKeyStr（sku 组合列表中当前类目对应的 key 值）
+        // 值：skuValueId（规格值 id）
+        s1: '30349',
+        s2: '1193',
+        // 初始选中数量
+        selectedNum: 3
+      },
+      sku: {
+        // 所有sku规格类目与其值的从属关系，比如商品有颜色和尺码两大类规格，颜色下面又有红色和蓝色两个规格值。
+        // 可以理解为一个商品可以有多个规格类目，一个规格类目下可以有多个规格值。
+        tree: [
+          {
+            k: '颜色', // skuKeyName：规格类目名称
+            v: [
+              {
+                id: '30349', // skuValueId：规格值 id
+                name: '红色', // skuValueName：规格值名称
+                imgUrl: 'https://img.yzcdn.cn/1.jpg' // 规格类目图片，只有第一个规格类目可以定义图片
+              },
+              {
+                id: '1215',
+                name: '蓝色',
+                imgUrl: 'https://img.yzcdn.cn/2.jpg'
+              }
+            ],
+            k_s: 's1' // skuKeyStr：sku 组合列表（下方 list）中当前类目对应的 key 值，value 值会是从属于当前类目的一个规格值 id
+          }
+        ],
+        // 所有 sku 的组合列表，比如红色、M 码为一个 sku 组合，红色、S 码为另一个组合
+        list: [
+          {
+            id: 2259, // skuId，下单时后端需要
+            price: 100, // 价格（单位分）
+            s1: '1215', // 规格类目 k_s 为 s1 的对应规格值 id
+            s2: '1193', // 规格类目 k_s 为 s2 的对应规格值 id
+            s3: '0', // 最多包含3个规格值，为0表示不存在该规格
+            stock_num: 110 // 当前 sku 组合对应的库存
+          }
+        ],
+        price: '1.00', // 默认价格（单位元）
+        stock_num: 227, // 商品总库存
+        collection_id: 2261, // 无规格商品 skuId 取 collection_id，否则取所选 sku 组合对应的 id
+        none_sku: false, // 是否无规格商品
+        messages: [
+          {
+            // 商品留言
+            datetime: '0', // 留言类型为 time 时，是否含日期。'1' 表示包含
+            multiple: '0', // 留言类型为 text 时，是否多行文本。'1' 表示多行
+            name: '留言', // 留言名称
+            type: 'text', // 留言类型，可选: id_no（身份证）, text, tel, date, time, email
+            required: '1' // 是否必填 '1' 表示必填
+          }
+        ],
+        hide_stock: false // 是否隐藏剩余库存
+      }
     }
   },
   components: {
@@ -35,10 +148,43 @@ export default {
       this.title = data[0].title
       this.rating = data[0].rating.average
     })
+  },
+  methods: {
+    chat () {
+      console.log('客服')
+    },
+    goCart () {
+      console.log('去购物车')
+    },
+    goShop () {
+      console.log('店铺')
+    },
+    addCart () {
+      console.log('加入购物车')
+      this.show = true
+    },
+    buy () {
+      console.log('立即购买')
+    },
+    back () {
+      this.$router.back()
+    },
+    share () {
+      console.log('分享')
+    }
   }
 }
 </script>
 
 <style lang="scss">
-
+.van-nav-bar {
+  background-color: #f66;
+  color: #fff;
+}
+.van-nav-bar__text, .van-nav-bar__title {
+  color: #fff;
+}
+.van-nav-bar .van-icon {
+    color: #fff;
+}
 </style>
--- day04/myapp/src/views/home/index.vue
+++ day04/myapp/src/views/home/index.vue
@@ -2,27 +2,69 @@
   <div class="container">
     <header class="header">首页头部</header>
     <div class="content">
-      <van-swipe :autoplay="3000" indicator-color="white">
-        <van-swipe-item v-for="(item, index) of bannerlist" :key = "index">
-          <img :src="item" alt="">
-        </van-swipe-item>
-      </van-swipe>
-      <Prolist :prolist = "prolist"/>
+      <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+        <van-swipe :autoplay="3000" indicator-color="white">
+          <van-swipe-item v-for="(item, index) of bannerlist" :key = "index">
+            <img :src="item" alt="">
+          </van-swipe-item>
+        </van-swipe>
+        <van-list
+          v-model="loading"
+          :finished="finished"
+          finished-text="没有更多了"
+          @load="onLoad"
+        >
+          <Prolist :prolist = "prolist"/>
+        </van-list>
+      </van-pull-refresh>
     </div>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
-import { Swipe, SwipeItem } from 'vant'
+import { Swipe, SwipeItem, List, PullRefresh } from 'vant'
 import Prolist from '@/components/common/Prolist'
 
 Vue.use(Swipe).use(SwipeItem)
+Vue.use(List)
+Vue.use(PullRefresh)
 export default {
   data () {
     return {
       bannerlist: [],
-      prolist: []
+      prolist: [],
+      loading: false, // 当组件滚动到底部时，会触发load事件并将loading设置成true
+      finished: false, // 当没有数据时，此值为true
+      pageNum: 1, // 页码
+      isLoading: false
+    }
+  },
+  methods: {
+    onRefresh () {
+      this.isLoading = true
+      fetch('https://www.daxunxun.com/douban').then(res => res.json()).then(data => {
+        this.isLoading = false // 下拉刷新结束
+        this.prolist = data // 重置列表的数据
+        this.pageNum = 1 // 重置页码 --- 下拉刷新相当于第一页数据
+        this.finished = false // 表示可以继续上拉加载下一页的数据
+      })
+    },
+    onLoad () {
+      console.log('可以加载数据了')
+      this.loading = true // 开始加载数据
+      fetch('https://www.daxunxun.com/douban?count=20&start=' + this.pageNum * 20)
+        .then(res => res.json()).then(data => {
+          this.loading = false // 数据加载完毕
+          this.pageNum++
+          if (data.length === 0) {
+            console.log('没有数据了')
+            this.finished = true // 没有数据了
+          } else {
+            // 有数据就要涉及到数字的合并   concat
+            this.prolist = [...this.prolist, ...data]
+          }
+        })
     }
   },
   components: {
